abdee6a68bca979da7ab933635433092e375f706
saibrindha <brindhaaswath@gmail.com>
2020-11-16 17:30:51 +0530

    resolved bug

--- services/registerservice.js
+++ services/registerservice.js
@@ -1,36 +1,43 @@
 class RegisterService {
     register = function (userObj) {
-        console.log(userObj) 
-        var usersTemp = JSON.parse(localStorage.getItem("USERS"));
-        var users = usersTemp ? usersTemp : [];
-        //validating emailaddress if already exists
-        let emailAlreadyExists = false;
-        console.log(users)
-        if (users.length == 0){
-            console.log("inside if")
-            users.push(userObj);
-            localStorage.setItem("USERS", JSON.stringify(users));
-            let result = "success";
-            return result;
+      console.log(userObj)
+      var usersTemp = JSON.parse(localStorage.getItem("USERS"));
+      var users = usersTemp ? usersTemp : [];
+      //validating emailaddress if already exists
+      let emailAlreadyExists = false;
+      console.log(users)
+      if (users.length == 0) {
+        console.log("inside if")
+        users.push(userObj);
+        localStorage.setItem("USERS", JSON.stringify(users));
+        let result = "success";
+        return result;
+      }
+  
+      let isPresent = false;
+  
+      for (let obj of users) {
+        console.log("insidefor")
+        if (userObj.email == obj.email) {
+          console.log(emailAlreadyExists)
+          emailAlreadyExists = true;
+          alert("Email already registered");
+          isPresent = true;
+          break;
         }
-        for (let obj of users) {
-            console.log("insidefor")
-            if (userObj.email == obj.email){
-                console.log(emailAlreadyExists)
-                emailAlreadyExists = true;
-                alert("Email already registered");
-                let result="fail";
-                return result;
-            }
-            else {
-                console.log("inside else")
-                users.push(userObj);
-                localStorage.setItem("USERS", JSON.stringify(users));
-                let result = "success";
-                return result;
-            
-            }
-        }
-
+      }
+  
+      if (isPresent == false) {
+        console.log("inside else")
+        users.push(userObj);
+        localStorage.setItem("USERS", JSON.stringify(users));
+        let result = "success";
+        return result;
+  
+      }
+      else if(isPresent == true) {
+        let result = "fail";
+        return result;
+      }
     }
-}
+  }
